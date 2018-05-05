bd671dc47c595a6a7f6c33dccd247b5c5f42d2a3
K. Eaglin <kceaglin@gmail.com>
2018-05-04 23:24:53 -0400

    refactoring modal with Materialize

--- src/components/Pay/Pay.js
+++ src/components/Pay/Pay.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import SquarePaymentForm from 'react-square-hosted-fields'
 
 const Pay = () => {
   return (
     <div>
+      <div>
       Hello from Pay component
+      </div>
+      <div>
+        <SquarePaymentForm appId="sq0idp-5tlOpPFwd6hAy27mAIknXQ" onNonceGenerated={this.handleNonce} />
+      </div>
     </div>
   )
 }
